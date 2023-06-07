61244dbe9f5de0ae74617a3e5d1c0a34d3844b3f
fensami <fensami2323@gmail.com>
2023-06-08 00:40:04 +0600

    editing slider

--- src/Pages/HomePage/PopularClassSection/PopularClassSection.jsx
+++ src/Pages/HomePage/PopularClassSection/PopularClassSection.jsx
@@ -2,50 +2,50 @@ import React from 'react';
 
 const PopularClassSection = () => {
   return (
-    <div>
-      <p className='text-5xl text-center font-bold text-[#38ada9] '>Popular Classes Section</p>
+    <div className='my-8'>
+      <p className='text-5xl text-center font-bold  text-[#38ada9] '>Popular Classes</p>
 
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-5 '>
-        <article className='flex flex-col'>
-          <img  src="https://media.gettyimages.com/id/521692458/photo/kids-soccer-training.jpg?s=612x612&w=gi&k=20&c=tka8ADfF_96IhZ0nt9lE2LzcBqouBseD6W3opIzoyZQ=" alt="" />
-          <p className='text-2xl font-semibold'>Basketball</p>
-          <p>students: 150</p>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5 mt-12 '>
+        <article className='flex flex-col shadow-xl border-2 rounded p-5'>
+          <img className='rounded-md h-full'  src="https://img.freepik.com/premium-photo/basketball-strategy-team-sports-coach-talking-usa-sport-group-about-coaching-game-court-young-athlete-students-communication-about-idea-during-training-professional-competition_590464-86954.jpg?w=2000" alt="" />
+          <p className='text-2xl my-3 font-semibold'>Basketball</p>
+          <p className='text-lg font-semibold'>Students: 120</p>
         </article>
-        <article className='flex flex-col'>
-          <img  src="https://media.gettyimages.com/id/521692458/photo/kids-soccer-training.jpg?s=612x612&w=gi&k=20&c=tka8ADfF_96IhZ0nt9lE2LzcBqouBseD6W3opIzoyZQ=" alt="" />
-          <p>Basketball</p>
-          <p>students: 150</p>
+        <article className='flex flex-col shadow-xl border-2 rounded p-5'>
+          <img className='rounded-md h-full'  src="https://media.gettyimages.com/id/501676602/photo/coach-giving-instruction-to-his-kids-soccer-team.jpg?s=612x612&w=gi&k=20&c=LP6ClbHS6BmEpGOz0TqVJA6wH9x619nawOcT3Q0ZwmA=" alt="" />
+          <p className='text-2xl my-3 font-semibold'>Football</p>
+          <p className='text-lg font-semibold'>Students: 150</p>
         </article>
-        {/* <article className='flex flex-row'>
-          <img  src="https://media.gettyimages.com/id/521692458/photo/kids-soccer-training.jpg?s=612x612&w=gi&k=20&c=tka8ADfF_96IhZ0nt9lE2LzcBqouBseD6W3opIzoyZQ=" alt="" />
-          <p>Basketball</p>
-          <p>students: 150</p>
+        <article className='flex flex-col shadow-xl border-2 rounded p-5'>
+          <img className='rounded-md h-full'  src="https://media.istockphoto.com/id/1421326043/photo/children-tennis-and-sport-with-a-group-of-friends-and-kids-on-a-sports-court-for-training.jpg?s=612x612&w=0&k=20&c=W85DQGevZ3NP4rYuIYBlg2wmnLqKo2zQ-k-RK-FBY7Q=" alt="" />
+          <p className='text-2xl my-3 font-semibold'>Tennis</p>
+          <p className='text-lg font-semibold'>Students: 50</p>
         </article>
-        <article className='flex flex-row'>
-          <img  src="https://media.gettyimages.com/id/521692458/photo/kids-soccer-training.jpg?s=612x612&w=gi&k=20&c=tka8ADfF_96IhZ0nt9lE2LzcBqouBseD6W3opIzoyZQ=" alt="" />
-          <p>Basketball</p>
-          <p>students: 150</p>
+        <article className='flex flex-col shadow-xl border-2 rounded p-5'>
+          <img className='rounded-md h-full'  src="https://aquastarswim.com.au/app/uploads/2022/01/210414-Aquastar-80-Cropped.jpg" alt="" />
+          <p className='text-2xl my-3 font-semibold'>Swimming</p>
+          <p className='text-lg font-semibold'>Students: 80</p>
         </article>
-        <article className='flex flex-row'>
-          <img  src="https://media.gettyimages.com/id/521692458/photo/kids-soccer-training.jpg?s=612x612&w=gi&k=20&c=tka8ADfF_96IhZ0nt9lE2LzcBqouBseD6W3opIzoyZQ=" alt="" />
-          <p>Basketball</p>
-          <p>students: 150</p>
+        <article className='flex flex-col shadow-xl border-2 rounded p-5'>
+          <img className='rounded-md h-full'  src="https://membersolutions.com/wp-content/uploads/2017/04/iStock_80106731_XXXLARGE-1.jpg" alt="" />
+          <p className='text-2xl my-3 font-semibold'>Martial Arts</p>
+          <p className='text-lg font-semibold'>Students: 80</p>
         </article>
-        <article className='flex flex-row'>
-          <img  src="https://media.gettyimages.com/id/521692458/photo/kids-soccer-training.jpg?s=612x612&w=gi&k=20&c=tka8ADfF_96IhZ0nt9lE2LzcBqouBseD6W3opIzoyZQ=" alt="" />
-          <p>Basketball</p>
-          <p>students: 150</p>
+        <article className='flex flex-col shadow-xl border-2 rounded p-5'>
+          <img className='rounded-md h-full'  src="https://www.bkgymswim.com.au/wp-content/uploads/2019/10/KH_064_BKids_040819-1260x630.jpg" alt="" />
+          <p className='text-2xl my-3 font-semibold'>Gymnastics</p>
+          <p className='text-lg font-semibold'>Students: 80</p>
         </article>
-        <article className='flex flex-row'>
-          <img  src="https://media.gettyimages.com/id/521692458/photo/kids-soccer-training.jpg?s=612x612&w=gi&k=20&c=tka8ADfF_96IhZ0nt9lE2LzcBqouBseD6W3opIzoyZQ=" alt="" />
-          <p>Basketball</p>
-          <p>students: 150</p>
+        <article className='flex flex-col shadow-xl border-2 rounded p-5'>
+          <img className='rounded-md h-full'  src="https://aquastarswim.com.au/app/uploads/2022/01/210414-Aquastar-80-Cropped.jpg" alt="" />
+          <p className='text-2xl my-3 font-semibold'>Swimming</p>
+          <p className='text-lg font-semibold'>Students: 80</p>
         </article>
-        <article className='flex flex-row'>
-          <img  src="https://media.gettyimages.com/id/521692458/photo/kids-soccer-training.jpg?s=612x612&w=gi&k=20&c=tka8ADfF_96IhZ0nt9lE2LzcBqouBseD6W3opIzoyZQ=" alt="" />
-          <p>Basketball</p>
-          <p>students: 150</p>
-        </article> */}
+       
+
+
+
+      
       </div>
     </div>
   );
---
+++ src/Pages/HomePage/PopularInstructorsSection/PopularInstructors.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PopularInstructors = () => {
+  return (
+    <div className='my-8'>
+      <p className='text-5xl text-center font-bold  text-[#38ada9] '>Popular Classes</p>
+    </div>
+  );
+};
+
+export default PopularInstructors;
