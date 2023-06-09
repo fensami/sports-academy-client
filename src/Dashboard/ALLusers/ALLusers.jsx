b493746883afc1d7606ac315da9f2c4d8ad11d91
fensami <fensami2323@gmail.com>
2023-06-10 01:25:30 +0600

    creating 3 role users

--- src/Dashboard/ALLusers/ALLusers.jsx
+++ src/Dashboard/ALLusers/ALLusers.jsx
@@ -8,77 +8,99 @@ import { FaTrashAlt, FaUserShield } from "react-icons/fa";
 const ALLusers = () => {
   const [axiosSecure] = useAxiousSecure();
   const { data: users = [], refetch } = useQuery(['users'], async () => {
-      const res = await axiosSecure.get('/users')
-      return res.data;
+    const res = await axiosSecure.get('/users')
+    return res.data;
   })
 
-  const handleMakeAdmin = user =>{
+  const handleMakeAdmin = user => {
     fetch(`http://localhost:5000/users/admin/${user._id}`, {
-        method: 'PATCH'
+      method: 'PATCH'
     })
-    .then(res => res.json())
-    .then(data => {
+      .then(res => res.json())
+      .then(data => {
         console.log(data)
-        if(data.modifiedCount){
-            refetch();
-            Swal.fire({
-                position: 'top-end',
-                icon: 'success',
-                title: `${user.name} is an Admin Now!`,
-                showConfirmButton: false,
-                timer: 1500
-              })
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: `${user.name} is an Admin Now!`,
+            showConfirmButton: false,
+            timer: 1500
+          })
         }
+      })
+  }
+  const handleMakeInstructor = user => {
+    fetch(`http://localhost:5000/users/instructor/${user._id}`, {
+      method: 'PATCH'
     })
-}
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: `${user.name} is an instructor Now!`,
+            showConfirmButton: false,
+            timer: 1500
+          })
+        }
+      })
+  }
 
-// const handleDelete = user => {
+  // const handleDelete = user => {
 
-// }
+  // }
 
-  // const {data: users = [], refetch} = useQuery(['users'], async() => {
-  //   const res = await fetch(`http://localhost:5000/users/admin/${users._id}`)
-  //   return res.json()
-
-  // })
   return (
     <>
-    <div>
-      <h1>All Users</h1>
-    </div>
-    <div className="overflow-x-auto w-full border">
-    <table className="table ">
-      {/* head */}
-      <thead className='bg-[#26a09c] text-white'>
-        <tr>
-          <th>#</th>
-          <th className='font-bold'>Name</th>
-          <th>Make Admin</th>
-          <th>Make Instructor</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* row 1 */}
-        {
-          users.map((user, index) =><tr
-          key={index}
-          >
-          <th>{index + 1}</th>
-          <td>{user.email}</td>
-          <td>
-            { user.role === 'admin' ? 'admin' :
-                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost bg-orange-600  text-white"><FaUserShield></FaUserShield></button> 
-                                    }
-                                   
-                                    </td>
-                                    <td>instructor</td>
-          
-                                <td><button onClick={() => handleDelete(user)} className="btn btn-ghost bg-red-600  text-white"><FaTrashAlt></FaTrashAlt></button></td>
-        </tr> )
-        }
+      <div>
+        <h1>All Users</h1>
+      </div>
+      <div className="overflow-x-auto w-full border">
+        <table className="table ">
+          {/* head */}
+          <thead className='bg-[#26a09c] text-white'>
+            <tr>
+              <th>#</th>
+              <th className='font-bold'>Name</th>
+              <th>Make Admin</th>
+              <th>Make Instructor</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {
+              users.map((user, index) => <tr
+                key={index}
+              >
+                <th>{index + 1}</th>
+                <td>{user.email}</td>
+                <td>
+                  {user.role === 'admin' ? 'admin' :
+                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost bg-orange-600  text-white"  ><FaUserShield></FaUserShield></button>
 
-        {/* {
+                  }
+
+                </td>
+                <td>
+                  {user.role === 'instructor' ? 'instructor' :
+                    <button onClick={() => handleMakeInstructor(user)} className="btn btn-ghost bg-orange-600  text-white"
+                    ><FaUserShield></FaUserShield></button>
+                  }
+
+                </td>
+                <td>instructor</td>
+
+                <td><button onClick={() => handleDelete(user)} className="btn btn-ghost bg-red-600  text-white"><FaTrashAlt></FaTrashAlt></button></td>
+              </tr>)
+            }
+
+            {/* {
           users.map((user,index) => <tr
           key={user._id}
           >
@@ -89,12 +111,12 @@ const ALLusers = () => {
           <td>delete</td>
         </tr> )
         } */}
-       
-        
-        
-      </tbody>
-    </table>
-  </div>
+
+
+
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
